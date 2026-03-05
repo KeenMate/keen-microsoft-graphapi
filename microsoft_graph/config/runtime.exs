b9c55b07ded4903c1cf0b05907c9d7b43101def2
Ondrej Valenta <ondrej.valenta@keenmate.com>
2026-03-05 17:40:52 +0100

    - insignificant change

--- microsoft_graph/config/runtime.exs
+++
@@ -1,8 +0,0 @@
-import Config
-
-if config_env() == :prod do
-  config :keen_microsoft_graphapi, :config,
-    tenant_id: System.fetch_env!("AZURE_TENANT_ID"),
-    client_id: System.fetch_env!("AZURE_CLIENT_ID"),
-    client_secret: System.fetch_env!("AZURE_CLIENT_SECRET")
-end
--- microsoft_graph/config/test.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-config :keen_microsoft_graphapi, :config,
-  tenant_id: "test-tenant-id",
-  client_id: "test-client-id",
-  client_secret: "test-client-secret"
